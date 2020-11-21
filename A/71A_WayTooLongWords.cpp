c2fc8b00907ae957fcee3827ac8e9886a715059e
wang-zixuan <wwxiaokucha@gmail.com>
2020-11-21 13:48:30 +0800

    20201121

--- A/71A_WayTooLongWords.cpp
+++ A/71A_WayTooLongWords.cpp
@@ -4,19 +4,20 @@
 using namespace std;
 
 int main() {
-	int n;
-	char c[101];
-	cin >> n;
-	while (n) {
-	    cin >> c;
-	    int len = strlen(c);
-	    if (len > 10) {
-		    cout << c[0] << len - 2 << c[len - 1] << endl;
+    int n;
+    char c[101];
+    cin >> n;
+    while (n) {
+        cin >> c;
+        int len = strlen(c);
+        if (len > 10) {
+            cout << c[0] << len - 2 << c[len - 1] << endl;
         }
         else {
-            cout << c << endl;
+            cout << c << endl;  
         }
         n--;
     }
     return 0;
+    print("None")
 }

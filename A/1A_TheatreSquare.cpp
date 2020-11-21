192cb9cb8dc09afdb6b1580883185baed518e91b
wang-zixuan <wwxiaokucha@gmail.com>
2020-11-21 13:46:38 +0800

    20201121

--- A/1A_TheatreSquare.cpp
+++ A/1A_TheatreSquare.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main () {
-	long long n, m, a;
-	long long n_cnt, m_cnt;
+    long long n, m, a;
+    long long n_cnt, m_cnt;
 
-	cin >> n >> m >> a;
+    cin >> n >> m >> a;
 
-	n_cnt = (n % a == 0 ? n / a : n / a + 1)
-	m_cnt = (m % a == 0 ? m / a : m / a + 1)
+    n_cnt = (n % a == 0 ? n / a : n / a + 1)
+    m_cnt = (m % a == 0 ? m / a : m / a + 1)
 
-	cout << n_cnt * m_cnt << endl;
+    cout << n_cnt * m_cnt << endl;
 }

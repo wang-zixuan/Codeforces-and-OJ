6005fac72ff83020bc6b8775229d59f973676de5
wang-zixuan <wwxiaokucha@gmail.com>
2020-11-21 13:55:53 +0800

    fix indentation problem

--- OJ/POJ1182.cpp
+++ OJ/POJ1182.cpp
@@ -6,65 +6,65 @@ const int maxn = 160000;
 int fa[maxn];
 
 void init() {
-	for (int i = 0; i < maxn; i++) {
-		fa[i] = i;
-	}
+    for (int i = 0; i < maxn; i++) {
+        fa[i] = i;
+    }
 }
  
 int find(int x) {
-	return x == fa[x] ? x : fa[x] = find(fa[x]);
+    return x == fa[x] ? x : fa[x] = find(fa[x]);
 }
 
 void unionSet(int x, int y) {
-	x = find(x); 
-	y = find(y);
+    x = find(x); 
+    y = find(y);
 
-	if (x == y)
-		return;
+    if (x == y)
+        return;
 
-	else {
-		fa[x] = y; 
-	}
+    else {
+        fa[x] = y; 
+    }
 }
 
 int main() {
-	init();
-	int n, k;
+    init();
+    int n, k;
 
-	scanf("%d%d", &n, &k);
-	int ans = 0;
+    scanf("%d%d", &n, &k);
+    int ans = 0;
 
-	while (k--) {
-		int a, b, c;
-		scanf("%d%d%d", &a, &b, &c);
+    while (k--) {
+        int a, b, c;
+        scanf("%d%d%d", &a, &b, &c);
 
-		if (b > n || c > n || b < 1 || c < 1) {
-			ans++;
-			continue;
-		}
+        if (b > n || c > n || b < 1 || c < 1) {
+            ans++;
+            continue;
+        }
 
-		if (a == 1) {
-			if (find(b + n) == find(c) || find(b + 2 * n) == find(c)) {
-				ans++;
-				continue;
-			}
+        if (a == 1) {
+            if (find(b + n) == find(c) || find(b + 2 * n) == find(c)) {
+                ans++;
+                continue;
+            }
 
-			unionSet(b, c);
-			unionSet(b + n, c + n);
-			unionSet(b + 2 * n, c + 2 * n);
-		}
+            unionSet(b, c);
+            unionSet(b + n, c + n);
+            unionSet(b + 2 * n, c + 2 * n);
+        }
 
-		else {
-			if (b == c || find(b) == find(c) || find(b + 2 * n) == find(c)) {
-				ans++;
-				continue;
-			}
+        else {
+            if (b == c || find(b) == find(c) || find(b + 2 * n) == find(c)) {
+                ans++;
+                continue;
+            }
 
-			unionSet(b + n, c);
-			unionSet(b + 2 * n, c + n);
-			unionSet(b, c + 2 * n);
-		}
-	}
+            unionSet(b + n, c);
+            unionSet(b + 2 * n, c + n);
+            unionSet(b, c + 2 * n);
+        }
+    }
 
-	printf("%d\n", ans);
+    printf("%d\n", ans);
 } 
